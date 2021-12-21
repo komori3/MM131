97b7d91f13c7be4370151802ee90729123c689ec
komori3 <yamai.fujino@gmail.com>
2021-12-21 22:34:46 +0900

    simulator

--- src/StopTheElves.cpp
+++ src/StopTheElves.cpp
@@ -175,10 +175,18 @@ template<typename T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return t
 template<typename T> bool chmin(T& a, const T& b) { if (a > b) { a = b; return true; } return false; }
 
 
-// general params
-//int grid_size;
-//int box_cost;
-//double elf_spawn_prob;
+
+constexpr char TREE = 'T';
+constexpr char BOX = 'b';
+constexpr char ELF = 'e';
+constexpr char ELF_PRESENT = 'E';
+constexpr char ELF_BOX = 'B';
+constexpr char PRESENT = 'P';
+constexpr char EMPTY = '.';
+
+constexpr int PRESENT_VALUE = 100;
+constexpr int dr[] = { 0, -1, 0, 1 };
+constexpr int dc[] = { 1, 0, -1, 0 };
 
 struct TestCase;
 using TestCasePtr = std::shared_ptr<TestCase>;
@@ -206,22 +214,248 @@ struct Point {
     }
 };
 
-struct Rect {
-    int x, y, w, h;
-    Rect(int x = 0, int y = 0, int w = 0, int h = 0) : x(x), y(y), w(w), h(h) {}
-    std::string stringify() const {
-        return format("Rect [x=%d, y=%d, w=%d, h=%d]", x, y, w, h);
+std::vector<Point> create_rhombus(int cx, int cy, int dist) {
+    std::vector<Point> poly;
+    int x = cx + dist, y = cy, dx = -1, dy = -1;
+    for (int i = 0; i < dist; i++) {
+        poly.emplace_back(x, y);
+        x += dx; y += dy;
     }
-    Rect get_outer_shell() const { return Rect(x - 1, y - 1, w + 2, h + 2); }
-    Rect get_inner_shell() const { return Rect(x + 1, y + 1, w - 2, h - 2); }
-    std::vector<Point> get_boundary_points() const {
-        std::vector<Point> points;
-        for (int j = 0; j < w - 1; j++) points.emplace_back(x + j, y);
-        for (int i = 0; i < h - 1; i++) points.emplace_back(x + w - 1, y + i);
-        for (int j = w - 1; j > 0; j--) points.emplace_back(x + j, y + h - 1);
-        for (int i = h - 1; i > 0; i--) points.emplace_back(x, y + i);
-        return points;
+    dy = 1;
+    for (int i = 0; i < dist; i++) {
+        poly.emplace_back(x, y);
+        x += dx; y += dy;
     }
+    dx = 1;
+    for (int i = 0; i < dist; i++) {
+        poly.emplace_back(x, y);
+        x += dx; y += dy;
+    }
+    dy = -1;
+    for (int i = 0; i < dist; i++) {
+        poly.emplace_back(x, y);
+        x += dx; y += dy;
+    }
+    return poly;
+}
+
+std::vector<Point> create_inner_rhombus(int cx, int cy, int dist) {
+    std::vector<Point> points({ {cx, cy} });
+    for (int d = 1; d < dist; d++) {
+        for (const auto& p : create_rhombus(cx, cy, d)) {
+            points.push_back(p);
+        }
+    }
+    return points;
+}
+
+std::vector<Point> create_rect(int x, int y, int w, int h) {
+    std::vector<Point> points;
+    for (int c = x; c < x + w - 1; c++) points.emplace_back(c, y);
+    for (int r = y; r < y + h - 1; r++) points.emplace_back(x + w - 1, r);
+    for (int c = x + w - 1; c > x; c--) points.emplace_back(c, y + h - 1);
+    for (int r = y + h - 1; r > y; r--) points.emplace_back(x, r);
+    return points;
+}
+
+std::vector<Point> create_inner_rect(int x, int y, int w, int h) {
+    std::vector<Point> points;
+    for (int r = y + 1; r < y + h - 1; r++) {
+        for (int c = x + 1; c < x + w - 1; c++) {
+            points.emplace_back(c, r);
+        }
+    }
+    return points;
+}
+
+struct Simulator {
+
+    Xorshift rnd;
+
+    int N;
+    int C;
+    double spawn_prob;
+    std::vector<int> spawn_idx;
+    std::vector<Point> spawn_pos;
+
+    char grid[32][32];
+    int num_turns;
+    int num_presents;
+    int score;
+    int money;
+
+    void set_seed(int seed) { rnd.set_seed(seed); }
+
+    void generate(TestCasePtr tc) {
+
+        num_turns = 0;
+
+        N = tc->grid_size;
+        C = tc->box_cost;
+        spawn_prob = tc->elf_spawn_prob;
+        money = tc->initial_money;
+
+        spawn_idx.resize(N * N + 1, -1);
+        for (int i = 1; i <= N * N; i++) {
+            double a = rnd.next_double();
+            if (a < spawn_prob) {
+                int p = rnd.next_int(N * 4 - 4);
+                spawn_idx[i] = p;
+            }
+            else {
+                spawn_idx[i] = -1;
+            }
+        }
+
+        for (int x = 0; x < N - 1; x++) spawn_pos.emplace_back(x, 0);
+        for (int y = 0; y < N - 1; y++) spawn_pos.emplace_back(N - 1, y);
+        for (int x = N - 1; x > 0; x--) spawn_pos.emplace_back(x, N - 1);
+        for (int y = N - 1; y > 0; y--) spawn_pos.emplace_back(0, y);
+
+        num_presents = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = tc->initial_grid[i][j];
+                if (grid[i][j] == PRESENT) num_presents++;
+            }
+        }
+
+    }
+
+    void move_elf(int r, int c, int d) {
+        char e = grid[r][c];
+        grid[r][c] = EMPTY;
+        r += dr[d];
+        c += dc[d];
+        if (r < 0 || r >= N || c < 0 || c >= N) {
+            if (e == ELF_PRESENT) num_presents--;
+            return;
+        }
+        if (grid[r][c] == BOX) {
+            grid[r][c] = ELF_BOX;
+        }
+        else if (grid[r][c] == PRESENT) {
+            grid[r][c] = ELF_PRESENT;
+        }
+        else {
+            grid[r][c] = e;
+        }
+    }
+
+    void find_move(int y, int x) {
+        // ç≈èâÇ…êiÇÒÇæï˚å¸ÇæÇØÉÅÉÇÇµÇƒ BFS
+        std::queue<int> qd, qr, qc;
+        qd.push(-1);
+        qr.push(y);
+        qc.push(x);
+        char e = grid[y][x];
+        std::vector<int> order({ 0, 1, 2, 3 });
+        std::vector<int> visited(N * N, 0);
+        int dirToBox = -1;
+        while (!qd.empty()) {
+            int d = qd.front(); qd.pop();
+            int r = qr.front(); qr.pop();
+            int c = qc.front(); qc.pop();
+            if (e == ELF && grid[r][c] == PRESENT) {
+                assert(d != -1);
+                move_elf(y, x, d);
+                return;
+            }
+            if (!visited[r * N + c]) {
+                visited[r * N + c] = 1;
+                shuffle_vector(order, rnd);
+                for (int dd = 0; dd < 4; dd++) {
+                    int dir = order[dd];
+                    int nr = r + dr[dir];
+                    int nc = c + dc[dir];
+                    if ((nc >= 0 && nc < N && nr >= 0 && nr < N) || e == ELF_PRESENT || e == ELF_BOX) {
+                        bool canMove = false;
+                        if (e == ELF) {
+                            if (grid[nr][nc] == EMPTY || grid[nr][nc] == PRESENT) canMove = true;
+                        }
+                        else {
+                            if (nc < 0 || nc >= N || nr < 0 || nr >= N) {
+                                move_elf(y, x, d == -1 ? dir : d);
+                                return;
+                            }
+                            if (grid[nr][nc] == EMPTY) canMove = true;
+                        }
+                        if (grid[nr][nc] == BOX && dirToBox == -1) {
+                            dirToBox = (d == -1 ? dir : d);
+                        }
+                        if (canMove) {
+                            if (d == -1) qd.push(dir);
+                            else qd.push(d);
+                            qr.push(nr);
+                            qc.push(nc);
+                        }
+                    }
+                }
+            }
+        }
+        if (dirToBox != -1 && e == ELF) {
+            move_elf(y, x, dirToBox);
+        }
+    }
+
+    bool simulate(const std::vector<Point>& box_points) {
+        num_turns++;
+
+        for (const auto [col, row] : box_points) {
+            assert(!(col <= 0 || col >= N - 1 || row <= 0 || row >= N - 1));
+            assert(grid[row][col] == EMPTY);
+            assert(money >= C);
+            grid[row][col] = BOX;
+            money -= C;
+        }
+
+        std::vector<Point> elves;
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (grid[r][c] == ELF || grid[r][c] == ELF_PRESENT || grid[r][c] == ELF_BOX) {
+                    elves.emplace_back(c, r);
+                }
+            }
+        }
+
+        for (const auto& pos : elves) {
+            find_move(pos.y, pos.x);
+        }
+
+        int p = spawn_idx[num_turns];
+        if (p >= 0) {
+            int check = 0;
+            while (true) {
+                check++;
+                if (check > N * 4) break;
+                auto [pc, pr] = spawn_pos[p];
+                if (grid[pr][pc] == EMPTY) {
+                    grid[pr][pc] = ELF;
+                    break;
+                }
+                p = (1 + p) % (N * 4 - 4);
+            }
+        }
+
+        money++;
+
+        score = PRESENT_VALUE * num_presents + money;
+
+        return !(num_turns == N * N || num_presents == 0);
+    }
+
+    void print(std::ostream& out) const {
+        std::ostringstream oss;
+        oss << num_turns << std::endl;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                oss << grid[i][j];
+            }
+            oss << std::endl;
+        }
+        out << oss.str();
+    }
+
 };
 
 struct State {
@@ -230,15 +464,16 @@ struct State {
     int box_cost;
     double elf_spawn_prob;
 
+    int turn;
     int money;
     char grid[32][32];
-
-    State() {}
 
     void init(TestCasePtr tc) {
         grid_size = tc->grid_size;
         box_cost = tc->box_cost;
         elf_spawn_prob = tc->elf_spawn_prob;
+
+        turn = 0;
         money = tc->initial_money;
         std::memcpy(grid, tc->initial_grid, sizeof(char) * 32 * 32);
     }
@@ -251,67 +486,131 @@ struct State {
         }
     }
 
-    int count(const Rect& roi, char type) const {
-        int res = 0;
-        for (int y = roi.y; y < roi.y + roi.w; y++) {
-            for (int x = roi.x; x < roi.x + roi.w; x++) {
-                res += (grid[y][x] == type);
-            }
-        }
-        return res;
-    }
+};
 
-    int query(std::istream& in, std::ostream& out, const std::vector<Point> box_points) {
-        assert(box_points.size() * box_cost <= money);
-        if (box_points.empty()) {
-            out << -1;
+int query(State& state, std::istream& in, std::ostream& out, const std::vector<Point>& box_points) {
+    if (box_points.empty()) {
+        out << -1;
+    }
+    else {
+        for (const auto [x, y] : box_points) {
+            out << y << ' ' << x << ' ';
         }
-        else {
-            for (const auto [x, y] : box_points) {
-                out << y << ' ' << x << ' ';
+    }
+    out << std::endl;
+    int elapsed_ms;
+    in >> elapsed_ms >> state.money;
+    state.load(in);
+    state.turn++;
+    return elapsed_ms;
+}
+
+int simulate(TestCasePtr tc, const std::vector<Point>& box_points, int seed) {
+    int grid_size = tc->grid_size;
+    int box_cost = tc->box_cost;
+    Simulator sim;
+    sim.set_seed(seed);
+    sim.generate(tc);
+    while (true) {
+        int money = sim.money;
+        std::vector<Point> query;
+        for (const auto [x, y] : box_points) {
+            if (money >= box_cost && sim.grid[y][x] == '.') {
+                query.emplace_back(x, y);
                 money -= box_cost;
             }
         }
-        out << std::endl;
-        int elapsed_ms;
-        in >> elapsed_ms >> money;
-        load(in);
-        return elapsed_ms;
+        bool res = sim.simulate(query);
+        if (!res) break;
     }
+    return sim.score;
+}
 
-};
+double multi_simulate(TestCasePtr tc, const std::vector<Point>& box_points, int num_trial) {
+    int score_sum = 0;
+    for (int seed = 0; seed < num_trial; seed++) {
+        int score = simulate(tc, box_points, seed);
+        score_sum += score;
+    }
+    return (double)score_sum / num_trial;
+}
+
+//#define LOCAL_TEST
 
 int main() {
 
-    TestCasePtr tc = std::make_shared<TestCase>(std::cin);
+#ifdef LOCAL_TEST
+    std::ifstream ifs("C:\\dev\\heuristic\\tasks\\MM131\\in\\3.in");
+    std::istream& in = ifs;
+#else
+    std::istream& in = std::cin;
+#endif
 
+    TestCasePtr tc = std::make_shared<TestCase>(in);
     int grid_size = tc->grid_size;
     int box_cost = tc->box_cost;
+
+    std::vector<Point> critical_points;
+    int num_trial = 10;
+    double best_score = -1.0;
+    {
+        // do nothing
+        double score = multi_simulate(tc, {}, num_trial);
+        dump("nothing", score);
+        if (chmax(best_score, score)) {
+            critical_points = {};
+        }
+    }
+    {
+        // rhombus
+        int dist = (grid_size - 3) / 2;
+        for (int y = 1 + dist; y + dist < grid_size - 1; y++) {
+            for (int x = 1 + dist; x + dist < grid_size - 1; x++) {
+                auto contour = create_rhombus(x, y, dist);
+                double score = multi_simulate(tc, contour, num_trial);
+                dump("rhombus", score);
+                if (chmax(best_score, score)) {
+                    critical_points = contour;
+                }
+            }
+        }
+    }
+    {
+        // rect
+        auto contour = create_rect(2, 2, grid_size - 4, grid_size - 4);
+        double score = multi_simulate(tc, contour, num_trial);
+        dump("rect", score);
+        if (chmax(best_score, score)) {
+            critical_points = contour;
+        }
+    }
+
 
     State state;
     state.init(tc);
 
-    Rect best_roi;
-    {
-        int max_presents = -1;
-        for (int y = 1; y + 4 < grid_size; y++) {
-            for (int x = 1; x + 4 < grid_size; x++) {
-                Rect roi(x + 1, y + 1, 3, 3);
-                dump(roi, state.count(roi, 'P'));
-                int num_presents = state.count(roi, 'P');
-                if (chmax(max_presents, num_presents)) {
-                    best_roi = roi;
-                    max_presents = num_presents;
-                }
+#ifdef LOCAL_TEST
+    Simulator simulator;
+    simulator.set_seed(1000000007);
+    simulator.generate(tc);
+
+    while (true) {
+        std::vector<Point> box_points;
+        int money = simulator.money;
+        for (const auto [x, y] : critical_points) {
+            if (money >= box_cost && simulator.grid[y][x] == '.') {
+                box_points.emplace_back(x, y);
+                money -= box_cost;
             }
         }
-        dump(best_roi, max_presents);
-        dump(best_roi.get_outer_shell().get_boundary_points());
+        bool res = simulator.simulate(box_points);
+        //dump(tester.score);
+        //tester.print(std::cerr);
+        if (!res) break;
     }
-
-    auto critical_points = best_roi.get_outer_shell().get_boundary_points();
-
-    for (int turn = 0; turn < grid_size * grid_size; turn++) {
+    dump(simulator.score);
+#else
+    while (state.turn < grid_size * grid_size) {
         std::vector<Point> box_points;
         int money = state.money;
         for (const auto [x, y] : critical_points) {
@@ -320,8 +619,9 @@ int main() {
                 money -= box_cost;
             }
         }
-        int elapsed_ms = state.query(std::cin, std::cout, box_points);
-        dump(turn, elapsed_ms);
+        int elapsed_ms = query(state, std::cin, std::cout, box_points);
     }
+#endif
 
+    return 0;
 }
